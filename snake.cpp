725fe6dd90033a1150e08e4a2cfa54f3c7abc124
rahulgoyal911 <rahulgoyal0.rg@gmail.com>
2019-02-16 10:40:00 +0530

    saved state

--- snake.cpp
+++ snake.cpp
@@ -2,12 +2,15 @@ using namespace std;
 #include<iostream>
 #include<vector>
 #include<bits/stdc++.h>
+#include<fstream>
 
 char board[80][80];
 vector<struct snake> s{4};
-int length=4;
+int length=0;
 int lastx,lasty;
 int score=0;
+bool death;
+char input;
 void eatFood();
 struct food{
     int x;
@@ -52,8 +55,29 @@ void print()
  }
 
 }
-void createSnake()
+void createSnake(char choice)
 {
+    if(choice=='n'){
+        //prev game
+        //read
+        ifstream ifs;
+        ifs.open("tempfile.txt");
+
+        int initial=0;
+        while(s[length-1].value!='T'){
+            ifs>>s[length].x;
+            ifs>>s[length].y;
+            ifs>>s[length++].value;
+            // s[length].y>>ifs;
+            // s[length++].value>>ifs;
+        }
+        length;
+        ifs.close();
+      //  cout<<"closed";
+    }
+    else if(choice=='m'){
+        //new game
+        //write
         s[0].x=40;
         s[0].y=40;
         s[0].value='H';
@@ -65,7 +89,13 @@ void createSnake()
         s[2].value='#';
         s[3].x=40;
         s[3].y=43;
-        s[3].value='T';
+        s[3].value='#';
+        s[4].x=40;
+        s[4].y=44;
+        s[4].value='T';
+        length=5;
+    }
+        
         for(int i=0;i<length;i++){
             board[s[i].x][s[i].y]=s[i].value;
         }
@@ -80,8 +110,13 @@ void moveup(){
     if(s[0].x==0){
         s[0].x+=79;
     }
-    s[0].x-=1;
+    else{
+        s[0].x-=1;
     s[0].y-=0;
+
+    if(s[0].x==s[1].x){
+       s[0].x+=1; 
+    }
     for(int i=length-1;i>1;i--){
          s[i].x=s[i-1].x;
          s[i].y=s[i-1].y;
@@ -111,6 +146,7 @@ void moveup(){
         if(s[0].y<0){
             s[0].y+=79;
         }
+    }
     system("clear");
     print();
 }
@@ -122,6 +158,12 @@ void movedown(){
     int tempval = s[0].value;
     s[0].x+=1;
     s[0].y+=0;
+
+    if(s[0].x==s[1].x){
+       s[0].x-=1; 
+    }
+    else{
+
     for(int i=length-1;i>1;i--){
          s[i].x=s[i-1].x;
          s[i].y=s[i-1].y;
@@ -150,6 +192,7 @@ void movedown(){
         if(s[0].y<0){
             s[0].y+=79;
         }
+    }
     system("clear");
     print();
 
@@ -165,7 +208,11 @@ void moveleft(){
     }
     s[0].x-=0;
     s[0].y-=1;
-    for(int i=length-1;i>1;i--){
+    if(s[0].y==s[1].y){
+       s[0].y+=1; 
+    }
+   else{
+         for(int i=length-1;i>1;i--){
          s[i].x=s[i-1].x;
          s[i].y=s[i-1].y;
     }
@@ -193,6 +240,7 @@ void moveleft(){
         if(s[0].y<0){
             s[0].y+=79;
         }
+   }
     system("clear");
     print();
 
@@ -206,7 +254,11 @@ void moveright(){
     int tempval = s[0].value;
     s[0].x-=0;
     s[0].y+=1;
-    for(int i=length-1;i>1;i--){
+    if(s[0].y==s[1].y){
+       s[0].y-=1; 
+    }
+    else{
+            for(int i=length-1;i>1;i--){
          s[i].x=s[i-1].x;
          s[i].y=s[i-1].y;
     }
@@ -234,6 +286,8 @@ void moveright(){
         if(s[0].y<0){
             s[0].y+=79;
         }
+    }
+    
     system("clear");
     print();
 
@@ -261,35 +315,79 @@ void eatFood()
     length++;
     s[length-2].value='#';
      board[s[length-2].x][s[length-2].y]='#';
+     system("clear");
      print();
     generateFood();
 }
+void checkdeath(){
+    for(int i=1;i<length;i++){
+        if(s[0].x==s[i].x && s[0].y==s[i].y){
+            input='q';
+        }
+    }
+    death=true;
+
+}
 int main()
 {
+    cout<<"press n to continue previous game and press m for new game \n";
+    char choice;
+    cin>>choice;
     f.x= rand() % 50+20; 
     f.y= rand() % 50+20;
 border();
-createSnake();
+createSnake(choice);
+system("clear");
 print();
 generateFood();
-char input;
 int gameon=1;
 while(gameon){
     cin>>input;
     if(input=='w'){
         moveup();
+        checkdeath();
     }
     else if(input=='s'){
         movedown();
+        checkdeath();
     }
     else if(input=='a'){
         moveleft();
+        checkdeath();
     }
     else if(input=='d'){
         moveright();
+        checkdeath();
     }
-    else if(input=='q'){
+    if(input=='q'){
         //quit;
+        if(death==true){
+             s[0].x=40;
+        s[0].y=40;
+        s[0].value='H';
+        s[1].x=40;
+        s[1].y=41;
+        s[1].value='#';
+        s[2].x=40;
+        s[2].y=42;
+        s[2].value='#';
+        s[3].x=40;
+        s[3].y=43;
+        s[3].value='#';
+        s[4].x=40;
+        s[4].y=44;
+        s[4].value='T';
+        length=5;
+        death=false;
+        }
+        fstream ost;
+        ost.open("tempfile.txt");
+        for(int i=0;i<length;i++){
+            ost<<s[i].x<<endl;
+            ost<<s[i].y<<endl;
+            ost<<s[i].value<<endl;
+        }
+        ost.close();
         cout<<"Your final score is "<<score<<endl;
         break;
     }
